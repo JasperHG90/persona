4b85640a03d77dd8383cbcc137749c4ea77ce3cb
Jasper Ginn <jasperginn@gmail.com>
2026-01-02 22:02:36 +0100

    chore: fix types

--- src/persona/types.py
+++ src/persona/types.py
@@ -1,13 +1,3 @@
-from aenum import Enum
 from typing import Literal
 
 personaTypes = Literal['roles', 'skills']
-
-
-class PersonaTypeEnum(Enum):  # type: ignore
-    ROLES = 'roles'
-    SKILLS = 'skills'
-
-
-def fn(x: PersonaTypeEnum) -> str:
-    return x.value
